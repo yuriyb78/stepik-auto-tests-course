463cb52e36292a04a0573c907c0f8d9dfc494382
yuriyb78 <byur78@mail.ru>
2021-12-02 18:04:59 +0300

    add file lesson_3.2_step_13.py

---
+++ lesson_3.2_step_13.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try: 
+    link = "http://suninjuly.github.io/math.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    #поиск значения Х и вычисление функции
+    x_element = browser.find_element_by_css_selector("#input_value.nowrap")
+    x = x_element.text
+    y = calc(x)
+
+    # заполняем поле ввода, выбор чек бокса и радиоботтон
+    input1 = browser.find_element_by_id("answer")
+    input1.send_keys(y)
+    check1 = browser.find_element_by_id("robotCheckbox")
+    check1.click()
+    radio1 = browser.find_element_by_id("robotsRule")
+    radio1.click()
+    
+   
+    # Отправляем заполненную форму
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
