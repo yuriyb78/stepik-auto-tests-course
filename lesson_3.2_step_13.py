3bd9ce078b3de6da69732d708eda9edc0142aa6b
yuriyb78 <byur78@mail.ru>
2021-12-03 12:46:15 +0300

    add class and first test of registrarion in lesson_3.2_step_13.py

--- lesson_3.2_step_13.py
+++ lesson_3.2_step_13.py
@@ -1,35 +1,46 @@
 from selenium import webdriver
+import unittest
 import time
-import math
 
-def calc(x):
-  return str(math.log(abs(12*math.sin(int(x)))))
 
-try: 
-    link = "http://suninjuly.github.io/math.html"
-    browser = webdriver.Chrome()
-    browser.get(link)
+class Test_Reg(unittest.TestCase):
 
-    #поиск значения Х и вычисление функции
-    x_element = browser.find_element_by_css_selector("#input_value.nowrap")
-    x = x_element.text
-    y = calc(x)
+    def test_reg1(self):
+        try:
+            link = "http://suninjuly.github.io/registration1.html"
+            browser = webdriver.Chrome()
+            browser.get(link)
 
-    # заполняем поле ввода, выбор чек бокса и радиоботтон
-    input1 = browser.find_element_by_id("answer")
-    input1.send_keys(y)
-    check1 = browser.find_element_by_id("robotCheckbox")
-    check1.click()
-    radio1 = browser.find_element_by_id("robotsRule")
-    radio1.click()
-    
-   
-    # Отправляем заполненную форму
-    button = browser.find_element_by_css_selector("button.btn")
-    button.click()
+            # Ваш код, который заполняет обязательные поля
+            input1 = browser.find_element_by_css_selector("div.first_block .first")
+            input1.send_keys("Ivan")
+            input2 = browser.find_element_by_css_selector("div.first_block .second")
+            input2.send_keys("Petrov")
+            input3 = browser.find_element_by_css_selector("div.first_block .third")
+            input3.send_keys("Smolensk")
 
-finally:
-    # ожидание чтобы визуально оценить результаты прохождения скрипта
-    time.sleep(10)
-    # закрываем браузер после всех манипуляций
-    browser.quit()
+            # Отправляем заполненную форму
+            button = browser.find_element_by_css_selector("button.btn")
+            button.click()
+
+            # Проверяем, что смогли зарегистрироваться
+            # ждем загрузки страницы
+            time.sleep(1)
+
+            # находим элемент, содержащий текст
+            welcome_text_elt = browser.find_element_by_tag_name("h1")
+            # записываем в переменную welcome_text текст из элемента welcome_text_elt
+            welcome_text = welcome_text_elt.text
+
+            # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+            self.assertEqual(welcome_text,"Congratulations! You have successfully registered!","Oops")
+
+        finally:
+            # ожидание чтобы визуально оценить результаты прохождения скрипта
+            time.sleep(2)
+            # закрываем браузер после всех манипуляций
+            browser.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
