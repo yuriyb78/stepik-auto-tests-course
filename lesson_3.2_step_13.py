4c355d65116175419b8610aecf8f32b3f9e4733c
yuriyb78 <byur78@mail.ru>
2021-12-03 12:59:18 +0300

    add class and second test of registrarion in lesson_3.2_step_13.py

--- lesson_3.2_step_13.py
+++ lesson_3.2_step_13.py
@@ -41,6 +41,40 @@ class Test_Reg(unittest.TestCase):
             # закрываем браузер после всех манипуляций
             browser.quit()
 
+    def test_reg2(self):
+        try:
+            link = "http://suninjuly.github.io/registration2.html"
+            browser = webdriver.Chrome()
+            browser.get(link)
+
+            # Ваш код, который заполняет обязательные поля
+            input1 = browser.find_element_by_css_selector("div.first_block .first")
+            input1.send_keys("Ivan")
+            input2 = browser.find_element_by_css_selector("div.first_block .third")
+            input2.send_keys("Smolensk")
+
+            # Отправляем заполненную форму
+            button = browser.find_element_by_css_selector("button.btn")
+            button.click()
+
+            # Проверяем, что смогли зарегистрироваться
+            # ждем загрузки страницы
+            time.sleep(1)
+
+            # находим элемент, содержащий текст
+            welcome_text_elt = browser.find_element_by_tag_name("h1")
+            # записываем в переменную welcome_text текст из элемента welcome_text_elt
+            welcome_text = welcome_text_elt.text
+
+            # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+            self.assertEqual(welcome_text,"Congratulations! You have successfully registered!","Oops")
+
+        finally:
+            # ожидание чтобы визуально оценить результаты прохождения скрипта
+            time.sleep(2)
+            # закрываем браузер после всех манипуляций
+            browser.quit()
+
 
 if __name__ == "__main__":
     unittest.main()
